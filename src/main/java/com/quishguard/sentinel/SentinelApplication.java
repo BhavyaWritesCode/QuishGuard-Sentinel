e06c983a141ffec07f64ebd9b3c6f9f11348dbdb
Bhavya Saini <bhavyasaini06@gmail.com>
2026-07-31 21:04:15 +0530

    feat: added pom.xml, structure, application.yml

---
+++ src/main/java/com/quishguard/sentinel/SentinelApplication.java
@@ -0,0 +1,13 @@
+package com.quishguard.sentinel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentinelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SentinelApplication.class, args);
+	}
+
+}
